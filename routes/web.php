2c82a0788608f643068656d07a890d64f5b68ed5
Davi Freitas <davifreitaz999@gmail.com>
2026-01-13 23:11:31 -0300

    atualiza configuração de domínios stateful no Sanctum e move rotas de autenticação para web.php

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,22 @@
 <?php
 
+use App\Http\Controllers\Auth\GoogleCallbackController;
+use App\Http\Controllers\Auth\GoogleRedirectController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\ProfileController;
+use App\Http\Controllers\Auth\RegisterController;
+use Illuminate\Support\Facades\Route;
 
-require __DIR__.'/api/auth.php';
+
+Route::prefix('auth')->group(function () {
+  Route::post('/register', RegisterController::class);
+  Route::post('/login', LoginController::class);
+  Route::get('google/redirect', GoogleRedirectController::class);
+  Route::get('google/callback', GoogleCallbackController::class);
+  
+  Route::middleware('auth')->group(function(){
+    Route::post('/logout', LogoutController::class);
+    Route::get('/profile',ProfileController::class);
+  });
+});
