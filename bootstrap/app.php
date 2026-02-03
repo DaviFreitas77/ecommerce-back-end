a3997010b4a57ebeece43e20a9770bc1a7598d8d
Davi Freitas <davifreitaz999@gmail.com>
2026-02-03 16:56:21 -0300

    config middleware pt-2

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,7 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
-        // $middleware->statefulApi();
+        $middleware->statefulApi();
         
         $middleware->alias([
             'is_admin' => IsAdmin::class,
